775b6997ee79ea1c8ab219b8c7d060d09cef69cf
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-07-16 03:38:16 +0000

    up

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -1,18 +1,69 @@
-import React from 'react'
+import { string } from "prop-types";
+import React from "react";
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
-const Counter = (props) => {
-    
-  return (
-    <div className="container-fluid d-flex bg-dark justify-content-center fs-1 ">
-            <div className="bg-dark bg-gradient text-white m-2 px-2 counter border border-secondary rounded "><i className="far fa-clock"></i></div>
-            <div className="bg-dark bg-gradient text-white m-2 px-2 counter border border-secondary rounded">{props.digiSix % 10}</div>
-            <div className="bg-dark bg-gradient text-white m-2 px-2 counter border border-secondary rounded">{props.digiFive % 10}</div>
-            <div className="bg-dark bg-gradient text-white m-2 px-2 counter border border-secondary rounded">{props.digiFour% 10}</div>
-            <div className="bg-dark bg-gradient text-white m-2 px-2 counter border border-secondary rounded">{props.digiThree % 10}</div>
-            <div className="bg-dark bg-gradient text-white m-2 px-2 counter border border-secondary rounded">{props.digiTwo % 10}</div>
-            <div className="bg-dark bg-gradient text-white m-2 px-2 counter border border-secondary rounded">{props.digiOne % 10}</div>
-        </div>
-  )
-}
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
 
-export default Counter
+
+
+const SecondsCounter = () => {
+
+	const [units, setUnits] = useState(0);
+	const [tens, setTens] = useState(0);
+	const [hundreds, setHundreds] = useState(0)
+	const [thousands, setThousands] = useState(0)
+	const [tThousands, setTthousands] = useState(0)
+	const [hThousands, setHthousands] = useState(0)
+
+	let timer;
+	useEffect(() => {
+		timer = setInterval(
+			function () {
+				setUnits(units + 1);
+				if (units === 9) {
+					setUnits(0);
+					setTens(tens + 1);
+				}
+				if (tens === 9) {
+					setTens(0);
+					setHundreds(hundreds + 1);
+				}
+				if (hundreds === 9) {
+					setHundreds(0);
+					setThousands(thousands + 1);
+				}
+				if (thousands === 9) {
+					setThousands(0);
+					setTthousands(tThousands + 1);
+				}
+				if (tThousands === 9) {
+					setTthousands(0);
+					setHthousands(hThousands + 1);
+				}
+				if (hThousands === 9) {
+					setHthousands(0);
+				}
+			}, 1000
+		)
+		return () => clearInterval(timer)
+	});
+
+
+	return (
+		<div className=" row bg-dark d-flex p-2 justify-content-between align-items-center">
+			<div className=" col p-0 m-3 bg-secondary fs-1" ><FontAwesomeIcon icon={faClock}></FontAwesomeIcon></div>
+			<div className=" col p-0 m-3 bg-secondary fs-1">{hThousands}</div>
+			<div className=" col p-0 m-3 bg-secondary fs-1">{tThousands}</div>
+			<div className=" col p-0 m-3 bg-secondary fs-1">{thousands}</div>
+			<div className=" col p-0 m-3 bg-secondary fs-1">{hundreds}</div>
+			<div className=" col p-0 m-3 bg-secondary fs-1">{tens}</div>
+			<div className=" col p-0 m-3 bg-secondary fs-1">{units}</div>
+
+		</div>
+	);
+};
+
+export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -5,19 +5,8 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-function SimpleCounter(props) {
-    return (
-        <div className="granC">
-            <div className="ca">
-                <i className="fa-regular fa-clock"></i>
-            </div>
-            <div className="cinco"></div>
-            <div className="cuatro"></div>
-            <div className="tres"></div>
-            <div className="dos"></div>
-            <div className="uno"></div>
-        </div>);
-}
+//import your own components
+import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+ReactDOM.render(<Home />, document.querySelector("#app"));
