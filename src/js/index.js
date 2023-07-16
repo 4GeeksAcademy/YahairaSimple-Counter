87aef06352f75508db11dddae3b3d604d3527b41
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-07-16 03:11:01 +0000

    up

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,19 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props) {
+    return (
+        <div className="granC">
+            <div className="ca">
+                <i className="fa-regular fa-clock"></i>
+            </div>
+            <div className="cinco"></div>
+            <div className="cuatro"></div>
+            <div className="tres"></div>
+            <div className="dos"></div>
+            <div className="uno"></div>
+        </div>);
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
